fb2a925b842edc17f22771b986ff724a572d9297
alexhdez98 <ahn1998@hotmail.com>
2019-10-19 16:34:35 +0200

    Results table

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
 import { PropTypes } from "prop-types";
+import { Table } from "react-bootstrap";
 
+import "./results.sass";
 import CustomButton from "../Buttons/Button";
+import { resetQuestions } from "../../services/apiCalls";
 
 const Results = props => {
-  const { history } = props;
+  const { history, questions, score, dispatch } = props;
 
   const buttonStyle = {
     background: "transparent",
@@ -15,16 +19,37 @@ const Results = props => {
     margin: "0 1em",
     padding: " 0.25em 1em"
   };
+
   const backButton = {
     name: "Go Back",
-    buttonFunction: () => history.push("/"),
+    buttonFunction: () => resetQuestions(dispatch) && history.push("/"),
     style: buttonStyle
   };
 
   return (
     <div>
-      <h1>PRUEBA</h1>
-      <CustomButton button={backButton} />
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Question</th>
+            <th>Answer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {questions.map((question, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{question.question}</td>
+              <td>{question.answer}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div className="table_score">
+        <td colSpan="3">Your Score is: {score}</td>
+        <CustomButton button={backButton} />
+      </div>
     </div>
   );
 };
@@ -33,4 +58,8 @@ Results.propTypes = {
   history: PropTypes.any.isRequired
 };
 
-export default Results;
+function mapStateToProps(state) {
+  return { ...state };
+}
+
+export default connect(mapStateToProps)(Results);
